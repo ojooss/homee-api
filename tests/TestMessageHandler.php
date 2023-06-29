4813518c5be365fa484694a396fc8fa0fc49aebc
ojooss <github@o-jooss.de>
2023-06-29 21:55:11 +0200

    Create TestMessageHandler.php

---
+++ tests/TestMessageHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HomeeApi\Tests;
+
+use HomeeApi\MessageHandlerInterface;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+
+class TestMessageHandler implements MessageHandlerInterface
+{
+
+    public array $stack = [];
+
+    public function __construct(
+        protected readonly string $logFile = __DIR__ . '/TestMessageHandler.txt',
+    ) {
+    }
+
+    public function handle(MessageInterface $message): void
+    {
+        // put message on stack
+        $this->stack[] = $message->getPayload();
+
+        // and write to file
+        file_put_contents(
+            $this->logFile,
+            print_r($message->getPayload(), true) . PHP_EOL . PHP_EOL . PHP_EOL,
+            FILE_APPEND
+        );
+    }
+}
