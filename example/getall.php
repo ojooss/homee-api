52da769be9f09cc994658ddb37e79dbf03ba8049
ojooss <github@o-jooss.de>
2023-07-07 22:12:25 +0200

    Update getall.php

--- example/getall.php
+++ example/getall.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/GetAllHandler.php';
 
-echo "*******************************" . PHP_EOL;
-echo "***     Listen to Homee     ***" . PHP_EOL;
-echo "*******************************" . PHP_EOL;
+echo "*************************************" . PHP_EOL;
+echo "***     Get all from to Homee     ***" . PHP_EOL;
+echo "*************************************" . PHP_EOL;
 
 $logger = new \Monolog\Logger('MyLogger');
 $streamHandler = new \Monolog\Handler\StreamHandler(__DIR__ . '/sample.log', \Monolog\Level::Debug);
